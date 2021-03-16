475108eb209abc638d6a5d02ba8c074d326aff1f
muskangupta1006 <78811268+muskangupta1006@users.noreply.github.com>
2021-03-16 17:49:54 +0530

    Add files via upload

---
+++ assignment 1/hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HELLO</title>
+    </head>
+    <body>
+        <h1>HELLO!  Welcome to github</h1>
+        <p>hello everyone</p>
+    </body>
+</html>
